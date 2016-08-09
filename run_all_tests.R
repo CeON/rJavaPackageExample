724fc7166d56d42292f650477257399573905932
Mateusz Kobos <mateusz@mkobos.com>
2016-08-09 17:44:47 +0200

    Fix `make test` doesn't work with an updated version of testthat

--- run_all_tests.R
+++ run_all_tests.R
@@ -3,12 +3,18 @@
 #' The script runs all tests in the project and returns status 1 if at least
 #' one of them failed,  0 otherwise.
 
+#' Code based on function `all_passed` from `reporter-list.R` file from 
+#' `testthat` package
+all_passed <- function(res) {
+  if (length(res) == 0)
+    return(TRUE)
+  df <- as.data.frame(res)
+  sum(df$failed) == 0 && all(!df$error)
+}
+
 #' Run tests
 res <- devtools::test()
-#' Extract a logical vector where each field corresponds to a separate test.
-#' A field is TRUE if the test passed, FALSE otherwise.
-test_passed <- unlist(sapply(res, function(x) sapply(x$results, function(y) y$passed)))
-all_tests_passed <- all(test_passed)
+all_tests_passed <- all_passed(res)
 
 #' Terminate R session with given status returned to shell
 if (all_tests_passed) {
